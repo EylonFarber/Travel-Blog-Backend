c9674b58319233a1c6385ef80e6ae7b4df52e98c
Eylon <eylonfar@gmail.com>
2025-08-02 16:26:51 +0200

    installing prisma and addapting the code for using prisma

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { PrismaClient } = require("./generated/prisma")
+const prisma = new PrismaClient()
+app.use(express.json());
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post(`/posts`, async (req, res) => {
+  
+    const {  author, title ,content,      
+  cover} = req.body
+    const result = await prisma.post.create({
+        data: {
+  author,
+  title ,
+  content,      
+  cover
+}
+    })
+    res.json(result)
+    })
+    
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
